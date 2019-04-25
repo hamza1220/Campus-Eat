ef8dd39767945d453c82a64ba7f98b2fff4429f4
razi9126@gmail.com <razi9126@gmail.com>
2019-04-25 20:08:24 +0500

    SS

--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -10,7 +10,6 @@ export default function(state = initialState, action ) {
     switch(action.type) {
         case SET_CURRENT_USER:
             // console.log(action.payload)
-            // console.log(action.id)
             return {
                 ...state,
                 isAuthenticated: !isEmpty(action.payload),
