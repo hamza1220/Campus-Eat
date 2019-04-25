1bbf8d6c2be2fc0b7dca6b11d9ab07e9b8480ee0
hktime9 <usama24march@gmail.com>
2019-04-25 23:23:24 +0500

    separate schemas

---
+++ models/Order.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    orderID: {
+        type: Number,
+        required: true
+    },
+    customer_email: {
+        type: String,
+        required: true
+    },
+    restaurant_name: {
+        type: String,
+        required: true
+    },
+    items: {
+        type: [ItemSchema],
+        required: true
+    },
+    order_time:{
+        type: Date,
+        default: Date.now
+    },
+    del_location:{
+        type: String,
+        required: true
+    },
+    del_time:{
+        type: Date,
+        default: Date.now 
+    },
+    status:{
+        type: String,
+        required:true
+    },
+    instructions:{
+        type: String,
+        required: true
+    }
+});
+
+const Order = mongoose.model('orders', OrderSchema);
+
+module.exports = Order;
