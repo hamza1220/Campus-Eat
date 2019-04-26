e6a700fe8be9c696c3c41f0aa30bdbfc097db801
hamza1220 <hamzaahmad86@gmail.com>
2019-04-27 01:24:09 +0500

    removeFromCart

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,4 +1,4 @@
-import { Button, Modal, InputGroup, FormControl, Form} from 'react-bootstrap';
+import { Button, Modal, InputGroup, FormControl, Form, Table} from 'react-bootstrap';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './menu.css'
@@ -17,7 +17,8 @@ class Menu extends Component {
  			show: false,
  			cart: [],
  			location:'',
- 			instructions: ''
+ 			instructions: '',
+ 			total: 0,
  		};
  	}
  	handleClose() {
@@ -30,6 +31,7 @@ class Menu extends Component {
 	}
 	placeOrder(event,email){
 		event.preventDefault();
+		
 		let loc =event.target.location.value
 		let inst =event.target.instruction.value
 
@@ -41,6 +43,8 @@ class Menu extends Component {
 			items:this.state.cart, del_location: loc, status: "pending", 
 			instructions: inst}
 		console.log(o)
+		this.setState({show:false});
+
 	}
 
 	submit(event){
@@ -70,6 +74,25 @@ class Menu extends Component {
 	{	e.preventDefault()
 		this.state.cart.push({item_id: id, name: name, price: price, category: cat, 
 			restaurant_name: this.state.rest})
+		let updatedPrice= this.state.total + price
+		this.setState({total: updatedPrice})
+	}
+
+	removeItemFromCart(e,item_id)
+	{
+		let remPrice = 0
+		for (var z = this.state.cart.length - 1; z >= 0; z--) {
+			if (this.state.cart[z].item_id===item_id)
+			{
+				remPrice=this.state.cart[z].price
+				break
+			}
+
+		}
+		let new_price = this.state.total- remPrice
+		this.state.cart.splice(z,1)
+		this.setState({total: new_price})
+		console.log(this.state.cart)
 	}
 
     render() {
@@ -88,16 +111,11 @@ class Menu extends Component {
     	}
 
     	let c = this.state.cart
-    	let total_price = 0;
-    	for (var i = c.length - 1; i >= 0; i--) {
-    		total_price = total_price + c[i].price
-    	}
-
-    	const cart_items = c.map((d,i)=> 
-    		<div>
-    		{d.name} {d.price}
-    		</div>
-    		)
+    	// let total_price = 0;
+    	// for (var i = c.length - 1; i >= 0; i--) {
+    	// 	total_price = total_price + c[i].price
+    	// }
+    	// this.setState({total: total_price})
 
     	const food_items = food.map((d,i)=> 
     		<div id="lol">
@@ -120,6 +138,16 @@ class Menu extends Component {
 		 	<button id='b2' onClick = {(e)=> {this.addToCart(e,d.item_id,d.name,d.price, d.category)}}> &nbsp; + &nbsp; &nbsp; </button>  
     		</div>
     	)
+    	
+    	const cart_table = c.map((d,i)=>
+    		<tr>
+	    		<td> {d.item_id} </td>
+	    		<td> {d.name} </td>
+	    		<td> {d.price} </td>
+	    		<td> <Button variant = "danger" onClick = {(e)=>{this.removeItemFromCart(e,d.item_id)}}> Remove </Button> </td>
+	    	</tr>
+    	)
+
         return (
             <div id="bg">
             <h1 id="heading">{this.state.rest}
@@ -142,21 +170,48 @@ class Menu extends Component {
 		          </Modal.Header>
 		          <Modal.Body>
 
-		          {cart_items}
-		          Total Order price: {total_price}
+		          <Table>
+		          	<thead>
+		          		<tr>
+			          		<th>Item ID</th>
+			          		<th>Name</th>
+			          		<th>Price</th>
+		          		</tr>
+		          	</thead>
+		          		<tbody>
+			          		{cart_table}
+			          	<tr> 
+			          		<td> </td>
+			          		<td> <b>Total Order Price</b> </td>
+			          		<td> <b>{this.state.total}</b> </td>
+			          	</tr>
+						</tbody>
+		          </Table>
+
 		         <form onSubmit={(e)=>{this.placeOrder(e,this.props.auth.user.email)}}>
+                    <label>
+                        Delivery Location &nbsp; &nbsp; &nbsp;  
                         <input
                         type="text"
                         placeholder="Location to deliver to"
                         name="location"
-                        /><input
+                        required
+                        />
+                    </label>
+                    <br/>
+
+                    <label>
+                    	Special Instructions &nbsp;
+                        <input
                         type="text"
                         placeholder="Instructions"
                         name="instruction"
                         />
-				  <button variant="primary" type="submit">
-				    Submit
-				  </button>
+                    </label>
+                    <br/>
+					<Button variant="danger" type="submit">
+					  Place Order
+					</Button>
 				</form>
 
 		          </Modal.Body>
