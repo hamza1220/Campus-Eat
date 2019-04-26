028866e4df2d374e2fe681060f6f74df597eda8c
hamza1220 <hamzaahmad86@gmail.com>
2019-04-26 15:14:51 +0500

    hamza

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const config = require('./db');
 const path = require('path');
 
 const User = require('./models/User');
-const Item = require('./models/Item')
+const Item = require('./models/Item');
+const Rest = require('./models/Rest');
+const Order= require('./models/Order')
 
 const users = require('./routes/user'); 
 
