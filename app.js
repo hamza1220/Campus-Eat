0c72fab9f2228e61fcbf3ba40a7a049c3d90d452
hamza1220 <hamzaahmad86@gmail.com>
2019-04-26 15:50:03 +0500

    menu-update

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.get('/api/rest-ratings', (req,res)=>{
 
 app.post('/api/gibprofile', (req,res)=>{
 	console.log("fetch data of ",req.body)
-	//GET FROM DB AND RETURN A JSON OBJECT
     User.findOne({
         email: req.body.email
     })
@@ -48,9 +47,7 @@ app.post('/api/gibprofile', (req,res)=>{
         }else{
         	res.json("LLG")
         }
-
-
-        });
+    });
     })
 
 app.post('/api/forgot-pw', (req,res)=>{
@@ -61,18 +58,23 @@ app.post('/api/forgot-pw', (req,res)=>{
 
 app.post('/api/menu', (req,res)=>{
 	console.log("send menu of",req.body)
-	//GET FROM DB AND RETURN A JSON OBJECT
-	res.json([{id:1, name: 'Chowmein', price: 250, category: "Food", restaurant: "ChopChop"},
-              {id:2, name: 'Rice', price: 100, category: "Food", restaurant: "ChopChop"},
-              {id:3, name: 'Coke', price: 50, category: "Drinks", restaurant: "ChopChop"}])
+	Item.find({
+        restaurant_name : "Food"
+    })
+    .then(items => {res.json(items)})
+
+	// res.json([{id:1, name: 'Chowmein', price: 250, category: "Food", restaurant: "ChopChop"},
+ //              {id:2, name: 'Rice', price: 100, category: "Food", restaurant: "ChopChop"},
+ //              {id:3, name: 'Coke', price: 50, category: "Drinks", restaurant: "ChopChop"}])
 })
+
 app.post('/additem', function(req, res) {
     const newItem= new Item({
     	item_id : req.body.item_id,
     	name : req.body.name,
     	price : req.body.price,
     	category : req.body.category,
-    	restaurant_name : req.body.category 
+    	restaurant_name : req.body.restaurant_name
     });
     newItem.save()
     .then(user=>{
