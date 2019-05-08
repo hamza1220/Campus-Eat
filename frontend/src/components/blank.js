f291616af0ed6f7b7551167b28519a5ea6c05bd9
razi9126@gmail.com <razi9126@gmail.com>
2019-05-08 17:05:14 +0500

    asasa

--- frontend/src/components/blank.js
+++ frontend/src/components/blank.js
@@ -1,62 +1,58 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { loginUser } from '../actions/authentication';
 import MetaTags from 'react-meta-tags';
-import StarRatings from 'react-star-ratings';
+import StarRatings from 'react-star-ratings'
+import './rest_rating.css'
 
+var restaurant_name = ''
+class blank extends Component {
+    constructor(props){
+         super(props);
 
+         this.state={
+      rating: -1,           
+        };
 
-class b2 extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            rating: 8,
-            
-        }
-        
-        
     }
 
-    
-componentDidMount(){
-        
-        const y= String(this.props.auth.user.user_type).split('_')[1]
-        fetch('api/get_rating', {
+    componentWillMount(){
+      restaurant_name = String(this.props.auth.user.user_type).split('_')[1]
+    fetch('api/get_rating', {
           method: 'POST',
-          body: JSON.stringify({rest: y}),
+          body: JSON.stringify({rest: restaurant_name}),
           headers: {
             "Content-Type": "application/json",
           }
+        }).then(res => res.json())
+        .then(body =>{
+            this.setState({rating: parseInt(body,10)})
         })
-        .then(res => {
-            res.json()
-            .then(body => {
-                this.setState({rating: parseInt(body,10)})
-            }); 
-        })
-    }
+  }
 
     render() {
-        const {errors} = this.state;
-        return(
-            <StarRatings
-          rating={this.state.rating}
-          starRatedColor="yellow"
-          numberOfStars={5}
-          name='rating'
-        />
-      );
-        
-        
+        return (
+            <div id = "rating">
+              &nbsp;
+              <br></br>
+              <br></br>
+              <h1>{restaurant_name}</h1>
+              <br></br><br></br>
+              <br></br>
+              <p> Your restaurant's current rating: </p>
+              <StarRatings
+                rating = {this.state.rating}
+                starRatedColor="#ffd700"
+                starDimension = "70px"
+                numberOfStars={5}
+                name = "rating"
+              />
+            </div>
+        );
     }
 }
 
 const mapStateToProps = (state) => ({
     auth: state.auth,
-    errors: state.errors
 })
 
-export default connect(mapStateToProps, { })(b2)
+export  default connect(mapStateToProps, {})(blank)
